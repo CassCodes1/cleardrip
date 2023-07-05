ed59741d690653b947ef10f1188eaca1c031c116
cass-dev27 <117110379+cass-dev27@users.noreply.github.com>
2023-07-05 09:38:39 +0100

    reflected changes (new routes and middleware)

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', require('./routes/mainRoutes'));
+app.use('/api/v1/users', require('./routes/userRoutes'));
 app.use('/api/v1', require('./routes/contactRoutes'));
-app.use('/api/v1', require('./routes/subscriberRoutes'));
+app.use('/api/v1', require('./routes/subscriptionRoutes'));
+app.use('/api/v1', require('./routes/adminRoutes/bookingRoutes.js'));
+app.use('/api/v1', require('./routes/adminRoutes/customerRoutes.js'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
