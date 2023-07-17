bc678787500ea296b35f755c105b2b4975861753
cass-dev27 <117110379+cass-dev27@users.noreply.github.com>
2023-07-17 11:29:03 +0100

    temporarily disabled error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
+// const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -17,7 +17,7 @@ app.use('/api/v1', require('./routes/subscriptionRoutes'));
 app.use('/api/v1', require('./routes/adminRoutes/bookingRoutes.js'));
 app.use('/api/v1', require('./routes/adminRoutes/customerRoutes.js'));
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
