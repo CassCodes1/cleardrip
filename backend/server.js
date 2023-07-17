244e3d2e13bbe52f050c0a2fe91001ad492fd28d
cass-dev27 <117110379+cass-dev27@users.noreply.github.com>
2023-07-17 12:15:54 +0100

    updated cors settings to allow cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://cleardrip-backend.onrender.com/',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
