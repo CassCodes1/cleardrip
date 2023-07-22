09694fc0c65ce534b8e37a414f549ae101e1dd17
cass-dev27 <117110379+cass-dev27@users.noreply.github.com>
2023-07-22 22:41:56 +0100

    completed ui for TabbedPanel component

--- frontend/cleardrip-website/src/pages/Services.jsx
+++ frontend/cleardrip-website/src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import cleardripVan from '../assets/images/cleardrip-van.jpg';
+import TabbedPanel from '../components/TabbedPanel/TabbedPanel';
 
 const Services = () => {
   return (
@@ -10,18 +11,29 @@ const Services = () => {
       </section>
       <section className='hero-image'>
         <div className='container'>
-          <img src={cleardripVan} alt='clear drip branded vehicle' />
+          <img
+            className='van'
+            src={cleardripVan}
+            alt='clear drip branded vehicle'
+          />
         </div>
       </section>
       <section className='page-info'>
         <div className='container'>
-          <div className='intro-text'>
+          <div className='intro-text services'>
             <h2>What do we do?</h2>
             <p>
               Whilst we primarily specialise in window cleaning, we also offer
-              gutter cleaning and pressure washing services.
+              gutter cleaning and pressure washing services. Click on the tabs
+              below for more information on our services.
             </p>
           </div>
+        </div>
+      </section>
+
+      <section className='services-info'>
+        <div className='container'>
+          <TabbedPanel />
         </div>
       </section>
     </>
