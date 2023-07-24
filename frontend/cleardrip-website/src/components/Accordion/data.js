b303e30b6bddac2ab0090446091ff8e6ff35e418
cass-dev27 <117110379+cass-dev27@users.noreply.github.com>
2023-07-24 13:57:45 +0100

    updated questions(FAQs)

--- frontend/cleardrip-website/src/components/Accordion/data.js
+++ frontend/cleardrip-website/src/components/Accordion/data.js
@@ -7,26 +7,21 @@ export const Data = [
   {
     title: 'Do you operate on bank holidays?',
     content:
-      'Absolutely! This is a very busy time for us, so we advise to book early to avoid disappointment',
+      'Absolutely! This is a very busy time for us, so we advise to book early to avoid disappointment.',
   },
   {
     title: 'Will you use chemicals on my windows?',
     content:
-      'We use 100% pure water, which is free from chemicals and limescale too!',
+      'We use 100% ultra pure water, which is free from chemicals and limescale too!',
   },
   {
-    title: 'Can I pay by card?',
+    title: 'What is ultra pure water?',
     content:
-      'Yes, we accept most payment methods including cash, card and bank transfer.',
+      "It's water with no impurities or mineral deposits present to leave any water marks whilst cleaning windows.",
   },
   {
-    title: 'Do you operate on bank holidays?',
+    title: 'Do you cover my area?',
     content:
-      'Absolutely! This is a very busy time for us, so we advise to book early to avoid disappointment',
-  },
-  {
-    title: 'Will you use chemicals on my windows?',
-    content:
-      'We use 100% pure water, which is free from chemicals and limescale too!',
+      "If you're unsure whether or not we cover your area, get in touch with us today and we'll be happy to help!",
   },
 ];
