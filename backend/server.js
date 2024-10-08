6b2ebd3011fa6fba82ed0342f92734ce4575fb80
CassCodes1 <117110379+CassCodes1@users.noreply.github.com>
2024-10-08 09:33:31 +0100

    updated cors options

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "https://cleardrip.netlify.app, https://cleardrip.netlify.app/*",
+    origin:
+      "https://cleardrip.netlify.app, https://cleardrip.netlify.app/, https://cleardrip.netlify.app/*",
     methods: "GET, POST, PUT, DELETE, OPTIONS",
+    optionsSuccessStatus: 200,
   })
 );
 app.use(express.json());
